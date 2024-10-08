766404dff256bfd532443ff7b03b1c1a5c845e6a
Daniil Kosyanov <kosyanov.daniil@inbox.ru>
2024-10-08 21:33:16 +0500

    Task7

---
+++ Tyuiu.KosyanovDV.Sprint2.Task7.V1.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.KosyanovDV.Sprint2.Task7.V1.Lib
+{
+    public class DataService : ISprint2Task7V1
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            bool condition1 = (Math.Pow(x, 2) + Math.Pow(y, 2)) <= 1;
+            bool condition2 = condition1 & (y>=-x) & (y>=x);
+            if (condition1 && condition2)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Tyuiu.KosyanovDV.Sprint2.Task7.V1.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.KosyanovDV.Sprint2.Task7.V1.Lib;
+namespace Tyuiu.KosyanovDV.Sprint2.Task7.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckDotInArea()
+        {
+            DataService ds = new DataService();
+            double x = 0.2;
+            double y = 0.6;
+            bool res = ds.CheckDotInShadedArea(x, y);
+            Assert.AreEqual(res, true);
+        }
+    }
+}
